303fbf3a3b4fc2056ab8d173d6f48c49c76a2e5b
Ay <ayobamioketona@gmail.com>
2026-07-30 20:06:55 +0100

    commit

--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,12 @@
-import app from '../server.ts';
+const importApp = async () => {
+  try {
+    const built = await import('../dist/server.cjs');
+    return built.default ?? built;
+  } catch (error) {
+    return (await import('../server.ts')).default;
+  }
+};
 
+const app = await importApp();
 export default app;
 
